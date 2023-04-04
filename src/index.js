31ae7d82483da11272c850d5e33a2da5dc65b267
Helcio <helcioanicio19@gmail.com>
2023-04-04 20:39:17 -0300

    Added the logic to calculator worked

--- src/index.js
+++ src/index.js
@@ -14,11 +14,63 @@ class Calculator {
     this.clear();
   }
 
+  delete() {
+    this.actualDisplay = this.actualDisplay.toString().slice(0, -1);
+  }
+
+  calculate () {
+    let result;
+
+    const _previewDisplay = parseFloat(this.previewDisplay);
+    const _actualDisplay = parseFloat(this.actualDisplay);
+
+    if (isNaN(_previewDisplay) || isNaN(_actualDisplay)) return;
+
+    switch (this.operator) {
+      case "+":
+        result = _previewDisplay + _actualDisplay;
+        break;
+
+      case "-":
+        result = _previewDisplay - _actualDisplay;
+        break;
+
+      case "/":
+        result = _previewDisplay / _actualDisplay;
+        break;
+
+      case "*":
+        result = _previewDisplay * _actualDisplay;
+        break;
+
+      default:
+        break;
+    }
+    // if (equal) {
+      
+    // } else {
+      
+    // }
+    this.actualDisplay = result;
+    this.operator = undefined;
+    this.previewDisplay = "";
+  }
+  
+  chooseOperation(operator) {
+    if (this.actualDisplay === "") return;
+
+    if (this.previewDisplay !== "") {
+      this.calculate();
+    }
+    this.operator = operator;
+    this.previewDisplay = `${this.actualDisplay} ${this.operator}`;
+    this.actualDisplay = "";
+  }
   
   appendNumber (number) {
     if (this.actualDisplay.includes('.') && number === '.') return;
 
-    this.actualDisplay = `${this.actualDisplay}${number.toString()}`
+    this.actualDisplay = `${this.actualDisplay}${number.toString()}`;
   }
 
   clear() {
@@ -31,33 +83,39 @@ class Calculator {
     this.previewDisplayText.innerText = this.previewDisplay;
     this.actualDisplayText.innerText = this.actualDisplay;
   }
-
-
-}
+};
 
 const calculator = new Calculator (
   previewDisplayText,
   actualDisplayText
-)
+);
 
 allClean.addEventListener('click', () => {
   calculator.clear();
   calculator.updateDisplay();
-})
+});
 
 numbers.forEach((number)=>{
   number.addEventListener('click', () => {
     calculator.appendNumber(number.innerText);
     calculator.updateDisplay()
   })
+});
+
+operators.forEach((operator)=>{
+  operator.addEventListener('click', () => {
+    calculator.chooseOperation(operator.innerText);
+    calculator.updateDisplay();
+  })
+});
+
+clean.addEventListener('click', () => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
+
+equal.addEventListener('click', () => {
+  calculator.calculate();
+  calculator.calculate();
+  calculator.updateDisplay();
 })
-
-// numbers.forEach((number) => {
-//   number.addEventListener('click', ()=> {
-//     const digit = parseInt (number.innerHTML)
-//     previewDisplay.innerHTML += digit;
-//   })
-// });
-
-
-
